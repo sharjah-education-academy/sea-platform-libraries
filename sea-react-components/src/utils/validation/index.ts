1bbd915b55da02b3ff4d0a0a36612439062be3b9
mohammed-masri <mqalmasri@sea.ac.ae>
2025-07-07 10:50:10 +0400

    optional phone number validation

--- sea-react-components/src/utils/validation/index.ts
+++ sea-react-components/src/utils/validation/index.ts
@@ -5,8 +5,11 @@ export const name = Yup.string().min(3).max(50).required("Required");
 export const birthDate = Yup.string().required("Required");
 export const email = Yup.string().email().required("Required");
 export const phoneNumber = Yup.string()
-  .matches(Utils.String.phoneRegExp, "Phone number is not valid")
-  .required("Required");
+  .optional()
+  .matches(Utils.String.phoneRegExp, {
+    excludeEmptyString: true,
+    message: "Phone number is not valid!!!",
+  });
 export const password = Yup.string().min(8).required("Required");
 export const confirmPassword = Yup.string()
   .oneOf([Yup.ref("password"), undefined], "Passwords must match")
