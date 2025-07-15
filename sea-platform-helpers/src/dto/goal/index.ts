8f4f72297d26a7dc39a4c82ff20f7cb94921eb10
Mahdi-sea <mmalmahdi@sn.ac.ae>
2025-07-15 11:35:45 +0400

    fix : update goal type

--- sea-platform-helpers/src/dto/goal/index.ts
+++ sea-platform-helpers/src/dto/goal/index.ts
@@ -1,8 +1,11 @@
+import { Organization } from "..";
+
 export interface IGoal {
-    id: string;
-    title: string;
-    description: string;
-    organizationId: string;
-    startYear: number;
-    endYear: number;
-  }
+  id?: string;
+  title: string;
+  description: string;
+  organizationId?: string;
+  organization?: Organization.IOrganization
+  startYear: number;
+  endYear: number;
+}
