8192e5668239cd6bf8ca23a3ab41fa5cb9de38f5
Mahdi-sea <mmalmahdi@sn.ac.ae>
2025-07-17 14:20:11 +0400

    feat : add animation when open modal

--- sea-react-components/tailwind.config.js
+++ sea-react-components/tailwind.config.js
@@ -2,5 +2,17 @@ module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
     plugins: [],
+    extend: {
+      animation: {
+        "fade-in": "fade-in 0.3s ease-out forwards",
+      },
+      keyframes: {
+        "fade-in": {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
+    },
+
   },
 };
