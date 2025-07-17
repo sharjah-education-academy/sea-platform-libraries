e61d0bda706b236a756b14c4cc49bc48fdbbf85f
mohammed-masri <mqalmasri@sea.ac.ae>
2025-07-17 13:01:30 +0400

    add onOpenChange to AccordionItem component

--- sea-react-components/src/components/accordion/index.tsx
+++ sea-react-components/src/components/accordion/index.tsx
@@ -5,6 +5,8 @@ import React, {
   useContext,
   ReactNode,
   useId,
+  useEffect,
+  useRef,
 } from "react";
 import Icon from "../icon";
 import clsx from "clsx";
@@ -50,23 +52,34 @@ export type AccordionItemProps = {
   title: ReactNode;
   children: ReactNode;
   id?: string;
+  onOpenChange?: (isOpen: boolean) => void;
 };
 
 export const AccordionItem: React.FC<AccordionItemProps> = ({
   title,
   children,
   id,
+  onOpenChange,
 }) => {
   const generatedId = useId();
   const itemId = id || generatedId;
   const { openItem, toggleItem } = useAccordion();
   const isOpen = openItem === itemId;
 
+  const prevIsOpenRef = useRef<boolean | null>(null);
+
+  useEffect(() => {
+    if (prevIsOpenRef.current !== isOpen) {
+      prevIsOpenRef.current = isOpen;
+      onOpenChange?.(isOpen);
+    }
+  }, [isOpen, onOpenChange]);
+
   return (
     <div className="group">
       <div
         onClick={() => toggleItem(itemId)}
-        className="w-full flex justify-between items-center gap-3 px-4 py-3 bg-white hover:bg-gray-50 transition text-left"
+        className="w-full flex justify-between items-center gap-3 px-4 py-3 bg-white hover:bg-primary/10 custom-animation text-left"
       >
         <div className="w-full">{title}</div>
 
@@ -79,7 +92,7 @@ export const AccordionItem: React.FC<AccordionItemProps> = ({
         />
       </div>
       <div
-        className={`px-4 pb-4 overflow-hidden transition-all duration-300 ease-in-out ${
+        className={`px-4 pb-4 overflow-auto transition-all duration-300 ease-in-out ${
           isOpen ? "max-h-screen opacity-100" : "max-h-0 opacity-0"
         }`}
       >
