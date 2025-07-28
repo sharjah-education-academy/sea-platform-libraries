5ed4721d530d77d22381f96590ecdca08ce7eb88
mohammed-masri <mqalmasri@sea.ac.ae>
2025-07-28 13:11:05 +0400

    add global constants

---
+++ sea-platform-helpers/src/constants/global/index.ts
@@ -0,0 +1,7 @@
+export type NoneType = "none";
+export const NoneValue: NoneType = "none";
+
+export type AllType = "all";
+export const AllValue: AllType = "all";
+
+export type IncludeQuery = string[] | AllType;
